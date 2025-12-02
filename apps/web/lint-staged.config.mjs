adb35bdbad6d68fb4f84abb41323017f7d1e8922
Dmitriy Mironov <dima.dev@icloud.com>
2025-12-02 01:52:10 +0000

    chore: refine lint-staged configuration, update ESLint command with timing

--- apps/web/lint-staged.config.mjs
+++ apps/web/lint-staged.config.mjs
@@ -2,6 +2,6 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  '*.{ts,tsx}': ['eslint --fix', 'prettier --write'],
   '*.{json,md,yml,yaml}': ['prettier --write'],
+  '*.{ts,tsx}': ['eslint --fix', 'prettier --write'],
 };
