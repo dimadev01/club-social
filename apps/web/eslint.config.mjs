45d95dbdf5a68c49905b3efd3cc2386e9a6cc409
Dmitriy Mironov <dima.dev@icloud.com>
2025-12-04 18:14:49 +0000

    chore: remove unused ESLint and Prettier dependencies

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,5 +1,5 @@
 import config from '@club-social/eslint-config';
-import pluginQuery from '@tanstack/eslint-plugin-query';
+import tanstackPluginQuery from '@tanstack/eslint-plugin-query';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig, globalIgnores } from 'eslint/config';
@@ -8,7 +8,7 @@ import globals from 'globals';
 export default defineConfig([
   config,
   globalIgnores(['dist']),
-  pluginQuery.configs['flat/recommended'],
+  tanstackPluginQuery.configs['flat/recommended'],
   {
     extends: [reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
     files: ['**/*.{ts,tsx}'],
