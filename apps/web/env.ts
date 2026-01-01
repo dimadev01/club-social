d60010b3abc6599cbe33eab3f5ac00331de6cbd7
Dmitriy Mironov <dima.dev@icloud.com>
2026-01-01 20:39:14 -0300

    chore: remove VITE_APP_DISPLAY_NAME from environment configuration

--- apps/web/env.ts
+++ apps/web/env.ts
@@ -3,7 +3,6 @@ import { defineConfig, Schema } from '@julr/vite-plugin-validate-env';
 export default defineConfig({
   schema: {
     VITE_APP_API_URL: Schema.string(),
-    VITE_APP_DISPLAY_NAME: Schema.string(),
   },
   validator: 'builtin',
 });
